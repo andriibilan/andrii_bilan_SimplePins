f892f4939133b05babf9bd7abf9808e18c1ed895
andriibilan <alfabetta09@gmail.com>
2018-02-23 18:53:28 +0200

    new project

---
+++ andrii_bilan_SimplePins/andrii_bilan_SimplePins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  andrii_bilan_SimplePins
+//
+//  Created by Andrii on 2/23/18.
+//  Copyright © 2018 Andrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
